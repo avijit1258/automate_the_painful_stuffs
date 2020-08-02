3befc0b426e05ef28596406d912e5e9f6f0f5722
avijit1258 <avijitbhatt1258@gmail.com>
2020-08-02 04:07:28 -0600

    fixes #2

--- caller_callee_extraction_pyan.sh
+++ caller_callee_extraction_pyan.sh
@@ -4,5 +4,11 @@
 # provide the address to pyan for extracting caller callee
 # get the output file with proper naming
 
-repository = $1
+repository=$1
+
+mkdir temp
+cd temp
+
+echo 'Cloning repo: $repository'
+git clone $repository
 
