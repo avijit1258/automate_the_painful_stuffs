15cdd22403a714970aca0c3fa28eea8d617535ba
avijit1258 <avijitbhatt1258@gmail.com>
2020-10-06 16:42:48 -0600

    java call graph usage instruction added.

--- caller_callee_extraction_pyan.py
+++ caller_callee_extraction_pyan.py
@@ -4,6 +4,9 @@
 # provide the address to pyan for extracting caller callee
 # get the output file with proper naming
 # python caller_callee_extraction_pyan.py 'https://github.com/avijit1258/pyan' '/home/avijit/Github/pyan/pyan.py'
+# python caller_callee_extraction_pyan.py 'https://github.com/scipy/scipy/' '/home/avijit/Github/pyan/pyan.py'
+# /home/avijit/Github/updated_pyan/pyan/pyan3 *.py   --uses --no-defines --colored --grouped --annotated  -f fastapi.txt --uses --no-defines --tgf
+
 import sys
 import os
 from datetime import datetime
@@ -25,7 +28,8 @@ target_repo = current_wd + "/" + repository.split('/')[4]
 
 output_file = repository.split('/')[4] + "_" +datetime.now().strftime("%m_%d_%Y")+ ".txt"
 
-execute_pyan = "python3 " + pyan + " " + target_repo+ "/**/*.py " + target_repo+ "/*.py --uses --no-defines --colored --grouped --annotated  -f " + output_file + " --uses --no-defines --tgf"
+# execute_pyan = "python3 " + pyan + " " + target_repo+ "/**/*.py " + target_repo+ "/*.py --uses --no-defines --colored --grouped --annotated  -f " + output_file + " --uses --no-defines --tgf"
+execute_pyan = "python3 " + pyan + " " + target_repo + " --uses --no-defines --colored --grouped --annotated  -f " + output_file + " --uses --no-defines --tgf"
 print(execute_pyan)
 
 os.system(execute_pyan)
