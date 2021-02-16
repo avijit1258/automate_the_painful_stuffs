b01dd47763febc6905f0612f37b34f0237c8196f
Avijit Bhattacharjee <avijitbhatt1258@gmail.com>
2021-02-15 19:04:04 -0600

    udpated to use pip pyan3 and use repo specific pattern

--- caller_callee_extraction_pyan.py
+++ caller_callee_extraction_pyan.py
@@ -6,14 +6,14 @@
 # python caller_callee_extraction_pyan.py 'https://github.com/avijit1258/pyan' '/home/avijit/Github/pyan/pyan.py'
 # python caller_callee_extraction_pyan.py 'https://github.com/scipy/scipy/' '/home/avijit/Github/pyan/pyan.py'
 # /home/avijit/Github/updated_pyan/pyan/pyan3 *.py   --uses --no-defines --colored --grouped --annotated  -f fastapi.txt --uses --no-defines --tgf
+# 15/02/2021 pyan3 'repo URL(https://github.com/user/project)' 'pattern(/**/*.py)' 
 
 import sys
 import os
 from datetime import datetime
 
 repository = sys.argv[1]
-pyan = sys.argv[2]
-
+repo_pattern = sys.argv[2]
 
 # os.system("mkdir temp")
 # os.system("cd temp")
@@ -23,13 +23,13 @@ print("Cloning repo: ", repository)
 git_clone = "git clone  "+ repository
 
 os.system(git_clone)
-current_wd = os.getcwd()
-target_repo = current_wd + "/" + repository.split('/')[4]
+current_wd = os.getcwd() 
+target_repo = current_wd + "/" + repository.split('/')[4] + pattern
 
 output_file = repository.split('/')[4] + "_" +datetime.now().strftime("%m_%d_%Y")+ ".txt"
 
 # execute_pyan = "python3 " + pyan + " " + target_repo+ "/**/*.py " + target_repo+ "/*.py --uses --no-defines --colored --grouped --annotated  -f " + output_file + " --uses --no-defines --tgf"
-execute_pyan = "python3 " + pyan + " " + target_repo + " --uses --no-defines --colored --grouped --annotated  -f " + output_file + " --uses --no-defines --tgf"
+execute_pyan = "pyan3 " + target_repo + " --uses --no-defines --colored --grouped --annotated  -f " + output_file + " --uses --no-defines --tgf"
 print(execute_pyan)
 
 os.system(execute_pyan)
